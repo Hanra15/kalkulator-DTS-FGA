50738ae96b0071d4cbc4ace5d244497cec71361f
Muhamad Farhan Rajab <64179056+Hanra15@users.noreply.github.com>
2023-04-03 15:09:16 +0700

    script

--- script.js
+++ script.js
@@ -1,91 +1,108 @@
-const calculatorScreen = document.querySelector(".calculator-screen")
-const numbers = document.querySelectorAll(".number")
-const operators = document.querySelectorAll(".operator")
-const equalSign = document.querySelector(".equal-sign")
-const clearBtn = document.querySelector(".all-clear")
-const decimal = document.querySelector(".decimal")
+const numbers = document.querySelectorAll(".number");
+const calculatorScreen = document.querySelector(".calculator-screen");
+const operators = document.querySelectorAll(".operator");
+const equalSign = document.querySelector(".equal-sign");
+const clearBtn = document.querySelector(".all-clear");
+const decimal = document.querySelector(".decimal");
 
-let prevNumber = ''
-let calculationOperator = ''
-let currentNumber = '0'
+// 3 variable
+let prevNumber = ""
+let calculatorOperator = ""
+let currentNumber = "0"
 
+// Mengambil nilai number
+numbers.forEach(number => {
+    number.addEventListener("click", (event) => {
+        inputNumber(event.target.value)
+        updateScreen(currentNumber);
+    })
+})
+
+// Merubah nilai ke screen
 const updateScreen = (number) => {
     calculatorScreen.value = number
 }
 
-numbers.forEach((number) =>{
-    number.addEventListener("click", (event) =>{
-        inputNumber(event.target.value)
-        updateScreen(currentNumber)
-    })
-    
-})
-
+// Memberikan number ke currentNumber
 const inputNumber = (number) => {
-    if(currentNumber === '0'){
+    if (currentNumber === "0") {
         currentNumber = number
-    }else{
+    } else {
         currentNumber += number
     }
 }
 
-inputDecimal = (dot) => {
-    if(currentNumber.includes('.')){
-        return
-    }
-    currentNumber += dot
-}
-
-decimal.addEventListener("click", (event) => {
-    inputDecimal(event.target.value)
-})
-
-operators.forEach((operator) => {
+// Mengambil nilai dari operator
+operators.forEach(operator => {
     operator.addEventListener("click", (event) => {
-        inputOperator(event.target.value)
+        inputOperator(event.target.value);
     })
 })
 
-const inputOperator = (operator) => {
-    if(calculationOperator === ''){
-        prevNumber = currentNumber
+// Memindahkan nilai ke operator
+const inputOperator = (operator => {
+    if (calculatorOperator === "") {
+        prevNumber = currentNumber;
     }
-    calculationOperator = operator
-    currentNumber = ''
-}
+    calculatorOperator = operator;
+    currentNumber = "0"
+})
 
-const calculate = () => {
-    let result = ''
-    switch(calculationOperator){
-        case "+":
-            result = parseFloat(prevNumber) + parseFloat(currentNumber)
-            break
-        case "-":
-            result = parseFloat(prevNumber) - parseFloat(currentNumber)
-            break
-        case "*":
-            result = parseFloat(prevNumber) * parseFloat(currentNumber)
-            break
-        case "/":
-            result = parseFloat(prevNumber) / parseFloat(currentNumber)
-            break
-    }
-    currentNumber = result
-    calculationOperator = ''
-}
-
+// Menambahkan logika pada tombol =
 equalSign.addEventListener("click", () => {
-    calculate()
-    updateScreen(currentNumber)
+    calculate();
+    updateScreen(currentNumber);
 })
 
+// Membuat function calculation
+const calculate = () => {
+    let result = ""
+    switch (calculatorOperator) {
+        case "+":
+            result = parseFloat(prevNumber) + parseFloat(currentNumber);
+            break;
+        case "-":
+            result = parseFloat(prevNumber) - parseFloat(currentNumber);
+            break;
+        case "*":
+            result = parseFloat(prevNumber) * parseFloat(currentNumber);
+            break;
+        case "/":
+            result = parseFloat(prevNumber) / parseFloat(currentNumber);
+            break;
+        case "%":
+            result = parseFloat(prevNumber) % parseFloat(currentNumber);
+            break;
+        default:
+            break;
+    }
+    currentNumber = result;
+    calculatorOperator = "";
+}
+
+// Fungsi untuk mereset nilai
 clearBtn.addEventListener("click", () => {
-    clearAll()
-    updateScreen(currentNumber)
+    clearAll();
+    updateScreen(currentNumber);
 })
 
+// Fungi reset
 const clearAll = () => {
-    let prevNumber = ''
-    let calculationOperator = ''
-    let currentNumber = '0'
+    prevNumber = "";
+    calculatorOperator = "";
+    currentNumber = "0";
+}
+
+// Kalkulasi decimal
+decimal.addEventListener("click", () => {
+    inputDecimal(event.target.value);
+    updateScreen(currentNumber);
+})
+
+// Menambahkan titik pada currentNumber
+inputDecimal = (dot) => {
+    if (currentNumber.includes(".")) {
+        return
+    }
+    currentNumber += dot
 }
